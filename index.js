4ac86e76cab55d20562cf69a343d5008d6345b6b
Jason Antman <antmanjt@gmail.com>
2022-03-19 14:10:52 -0400

    commit tree 33c11128c416f1db9cb5f3840eb1f60affb6b202

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(win) {
+    let year = win.year;
+    if (win.result === "W") {
+      return year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const finder = record.find(superbowlWin);
